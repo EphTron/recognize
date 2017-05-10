4e127a42c0dec727796ee444777f409627a4f0ae
EphTron <es.90@gmx.de>
2017-05-10 11:59:11 +0200

    added recognizer

---
+++ lib/Recognizer.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: hg3n
+"""
+
+from sklearn import svm, metrics
+import numpy as np
+
+
+class Recognizer:
+    def __init__(self, datasets=None):
+        """
+        c'tor
+        :param datasets:
+        """
+        # create classifier
+        self.classifier = svm.SVC(gamma=0.001)
+
+        # initialize if dataset is available
+        if datasets != None:
+            self.initialize(datasets)
+
+    def initialize(self, datasets):
+        """
+        initialize shape detector, shaping the data, learning according to input datasets
+        :param datasets:
+        :return:
+        """
+        self.datasets = datasets
+        images = join_datasets([d.images for d in datasets])
+        targets = join_datasets([d.targets for d in datasets])
+
+        # determine number of samples
+        n_samples = len(images)
+
+        # reshape data
+        data = images.reshape((n_samples, -1))
+
+        # learn from dataset
+        self.classifier.fit(data, targets)
+
+    def predict(self, targets):
+        """
+        predict input targets using svc's predict function
+        :param targets:
+        :return:
+        """
+        return self.classifier.predict(targets)
+
+    def report(self, targets, results):
+        """
+        returns the classifier and a generated classification report
+        :param targets: input list of targets to predict (need to be learned first)
+        :param results: input list of predicted elements
+        :return:
+        """
+        report = metrics.classification_report(targets, results)
+        return self.classifier, report
+
+
+def join_datasets(datasets):
+    """
+    join datasets into one
+    :param datasets:
+    :return:
+    """
+    first = datasets[0]
+    for i in range(1, len(datasets)):
+        first += datasets[i]
+    return np.asarray(first)
