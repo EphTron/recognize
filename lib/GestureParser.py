306323114f85ccf551d0fc870723df42e2da9054
EphTron <es.90@gmx.de>
2017-05-22 20:26:40 +0200

    added __lt__ function to points (for comparision), removed for loops for more performance, adjusted image creation, polygon fills holes in drawing now

--- lib/GestureParser.py
+++ lib/GestureParser.py
@@ -3,6 +3,10 @@ import math
 import numpy as np
 from enum import Enum
 import scipy.misc
+from skimage.draw import line, polygon_perimeter
+from os import path
+
+import matplotlib.pyplot as plt
 
 
 class Point:
@@ -18,6 +22,9 @@ class Point:
     def __str__(self):
         return "<Point id: " + str(self.id) + " _x: " + str(self.x) + " y: " + str(self.y) + ">"
 
+    def __lt__(self, other):
+        return self.x == other.x or self.y == other.y
+
 
 class ScaleMode(Enum):
     NO_SCALE = 1
@@ -28,7 +35,6 @@ class GestureParser:
     def __init__(self, SCALE_MODE, IMAGE_DIMENSION):
         """
         c'tor
-        :param POINT_LIST:
         :param SCALE_MODE:
         :param IMAGE_DIMENSION:
         """
@@ -36,17 +42,34 @@ class GestureParser:
         self.img_dim = IMAGE_DIMENSION
 
     def open_gpl_file(self, file_name):
+        """
+        obviously opens a file
+        :param file_name: 
+        :return: 
+        """
         result = [line.rstrip('\n') for line in open(file_name)]
         return result
 
     def prepare_lines(self, lines):
+        """
+        do complicated line stuff
+        :param lines: 
+        :return: 
+        """
         ret = []
         for line in lines:
             ret.append([float(x) for x in line.split(' ')])
         return ret
 
-    def convert_gpl_to_pointlist(self, path, file_name):
-        points = self.prepare_lines(self.open_gpl_file(path + file_name))
+    def convert_gpl_to_pointlist(self, folder, file_name):
+        """
+        load gpl file and return list of points
+        :param path: filepath
+        :param file_name: name of file
+        :return: 
+        """
+        final_path = path.join(folder, file_name)
+        points = self.prepare_lines(self.open_gpl_file(final_path))
         point_list = [Point(p[0], p[1]) for p in points]
         return point_list
 
@@ -80,35 +103,42 @@ class GestureParser:
         max_distance_y = math.fabs(min_y) + math.fabs(max_y)
 
         # create new internet (aka image)
-        image = [[0 for j in range(0, self.img_dim)] for i in range(0, self.img_dim)]
+        image = np.zeros((self.img_dim, self.img_dim))
 
-        p_x = 0
-        p_y = 0
-        for row_idx in range(len(image)):
-            for col_idx in range(len(image[0])):
-                for p in point_list:
+        p_x, p_y = 0, 0
+        x_list, y_list = [], []
+        for p in point_list:
 
-                    # scale the points to the maximum
-                    if self.scale_mode == ScaleMode.SCALE_MAX:
+            # scale the points to the maximum
+            if self.scale_mode == ScaleMode.SCALE_MAX:
 
-                        p_x = int(map_to(p.x, min_x, max_x, 0, self.img_dim))
-                        p_y = int(map_to(p.y, min_y, max_y, 0, self.img_dim))
+                p_x = int(map_to(p.x, min_x, max_x, 0, self.img_dim - 1))
+                p_y = int(map_to(p.y, min_y, max_y, 0, self.img_dim - 1))
 
-                    # keep image dimensions and align the detected gesture to the left hand side
-                    elif self.scale_mode == ScaleMode.NO_SCALE:
+            # keep image dimensions and align the detected gesture to the left hand side
+            elif self.scale_mode == ScaleMode.NO_SCALE:
 
-                        if max_distance_x > max_distance_y:
-                            ratio = (max_distance_y * 100) / max_distance_x
-                            p_x = int(map_to(p.x, min_x, max_x, 0, self.img_dim))
-                            p_y = int(map_to(p.y, min_y, max_y, 0, self.img_dim) * ratio / 100)
-                        else:
-                            ratio = (max_distance_x * 100) / max_distance_y
-                            p_x = int(map_to(p.x, min_x, max_x, 0, self.img_dim) * ratio / 100)
-                            p_y = int(map_to(p.y, min_y, max_y, 0, self.img_dim))
+                if max_distance_x > max_distance_y:
+                    ratio = (max_distance_y * 100) / max_distance_x
+                    p_x = int(map_to(p.x, min_x, max_x, 0, self.img_dim - 1))
+                    p_y = int(map_to(p.y, min_y, max_y, 0, self.img_dim - 1) * ratio / 100)
+                else:
+                    ratio = (max_distance_x * 100) / max_distance_y
+                    p_x = int(map_to(p.x, min_x, max_x, 0, self.img_dim - 1) * ratio / 100)
+                    p_y = int(map_to(p.y, min_y, max_y, 0, self.img_dim - 1))
 
-                    # write black pixel if the indices match
-                    if (p_x == row_idx and p_y == col_idx):
-                        image[row_idx][col_idx] = 255
+            # if the pixels are within the range save them to the list
+            if (p_x < 50 and p_y < 50):
+                x_list.append(p_x)
+                y_list.append(p_y)
+
+        # create polygon perimeter
+        rr, cc = polygon_perimeter(y_list, x_list, (self.img_dim, self.img_dim), clip=True)
+        image[rr, cc] = 255
+
+        # remove the connection between first and last point
+        # rl, cl = line(y_list[0], x_list[0], y_list[-1], x_list[-1])
+        # image[rl, cl] = 0
 
         # cast image to np array and return
         return np.asarray(image)
